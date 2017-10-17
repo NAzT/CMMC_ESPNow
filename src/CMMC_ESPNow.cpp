cb47f184560314235128bd7b150571ad90fd2f33
Nat <nat@cmmc.io>
2017-10-17 14:24:49 +0700

    update keywords.

--- src/CMMC_ESPNow.cpp
+++ src/CMMC_ESPNow.cpp
@@ -11,8 +11,14 @@ void CMMC_ESPNow::init(int mode) {
 		WiFi.disconnect();
 		Serial.println("Initializing ESPNOW...");
 		Serial.println("Initializing... SLAVE");
+		esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
 	}
-	
+	else {
+		WiFi.disconnect();
+		WiFi.mode(WIFI_STA);
+		delay(50);
+	}
+
 	if (esp_now_init() == 0) {
 		Serial.println("init");
 	} else {
@@ -20,11 +26,18 @@ void CMMC_ESPNow::init(int mode) {
 		ESP.restart();
 		return;
 	}
-	esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+
+	if (mode == NOW_MODE_CONTROLLER) {
+		esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+	} else {
+		esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+	}
+
 	esp_now_register_send_cb([](uint8_t* macaddr, uint8_t status) {
 		Serial.printf("sent status => [%lu]\r\n", status);
 		// utils.printMacAddress(macaddr);
 	});
+
 	esp_now_register_recv_cb([](uint8_t *macaddr, uint8_t *data, uint8_t len) {
 		//      if (digitalRead(BUTTON_PIN) == HIGH) {
 		// PACKET_T pkt;
@@ -35,4 +48,4 @@ void CMMC_ESPNow::init(int mode) {
 		// digitalWrite(LED, ledState);
 		// ledState = !ledState;
 	});
-}
+};
--- src/CMMC_ESPNow.h
+++ src/CMMC_ESPNow.h
@@ -16,14 +16,34 @@ extern "C" {
 
 class CMMC_ESPNow
 {
-public:
-    // constructure
-    CMMC_ESPNow() {}
-    ~CMMC_ESPNow() {}
-    void init(int mode);
-    void send(uint8_t *mac, u8* data, int len) {
-      esp_now_send(mac, (u8*) &data, len);
-    }
+  public:
+      // constructor
+      CMMC_ESPNow() {
+        esp_now_recv_cb_t dummy_recv;
+        esp_now_send_cb_t dummy_sent;
+        this->_user_on_message_recv = dummy_recv;
+        this->_user_on_message_sent = dummy_sent;
+      }
+      ~CMMC_ESPNow() {}
+      void init(int mode);
+      void send(uint8_t *mac, u8* data, int len) {
+        esp_now_send(mac, (u8*) &data, len);
+      }
+
+      void on_message_recv(esp_now_recv_cb_t cb) {
+        if (cb != NULL) {
+          this->_user_on_message_recv = cb;
+        }
+      }
+
+      void on_message_sent(esp_now_send_cb_t cb) {
+        if (cb != NULL) {
+          this->_user_on_message_sent = cb;
+        }
+      }
+  private:
+    esp_now_recv_cb_t _user_on_message_recv;
+    esp_now_send_cb_t _user_on_message_sent;
 };
 
 #endif //CMMC_ESPNow_H
